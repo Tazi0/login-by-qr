d1bb7cd9058014b777413759c28c34b5e2a2372f
Tazio <tazio2003@gmail.com>
2020-02-21 13:20:45 +0100

    Qr code

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
 require "config/db.php";
+include "phpqrcode.php";
+
+QRcode::png('https://', 'qrcode.png', 'Q', 4);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR code</title>
+</head>
+<body>
+    <img src="qrcode.png" alt="QR CODE">
+</body>
+</html>
